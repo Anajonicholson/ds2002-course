1ddfe50955e9a56683bae17bc2368af1a83a3bd9
Anajonicholson <142844681+Anajonicholson@users.noreply.github.com>
2024-02-27 13:04:16 -0500

    Adding lab3 & 4 manually

---
+++ mywork/labs/lab3/env-vars.py
@@ -0,0 +1,16 @@
+#!/C:/Users/16073/AppData/Local/Microsoft/WindowsApps/python.exe
+
+
+import os
+
+os.environ["EYE_COLOR"] = "Blue"
+os.environ["FAV_COLOR"] = "Purple"
+os.environ["FAV_FOOTBALL"] = "Steelers"
+
+EYE_COLOR = input('What is your eye color?')
+FAV_COLOR = input('What is your favorite color?')
+FAV_FOOTBALL = input('What is your favorite football team?')
+
+print(EYE_COLOR)
+print(FAV_COLOR)
+print(FAV_FOOTBALL)
---
+++ mywork/labs/lab3/madlib.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me a noun: " NOUN1
+read -p "3. Please give me a verb: " VRB1
+read -p "4. Please give me an adverb: " ADVRB1
+read -p "5. Please give me another adjective: " ADJ2
+read -p "6. Please give me another noun: " NOUN2
+read -p "7. Please give me another verb: " VRB2
+read -p "8. Please give me another adverb: " ADVRB2
+echo "Once upon a time, there was a $ADJ1 fox who $ADVERB1 $VRB1 through the field, longing to find his $NOUN1. This fox couln't find his $NOUN1 in the field, but rather found his friend Mike, the $ADJ2 bunny. Mike and the fox decided they wanted to $VRB2 to the $NOUN2 to grab a snack for lunch. They were both very hungry, so they did this $ADVRB2."
+
